ed254b15c9bc933a70bfc04d96cb2bc8e53ca1f3
Kemmachat <kemmachat@Kemmachats-MacBook-Air.local>
2018-09-05 00:39:33 +0700

    Veutify Vue Router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,8 +15,15 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+
+Vue.component('AppHome', require('./components/AppHome.vue'));
+import router from './Router/router.js'
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    router
 });
